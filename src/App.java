906ccf8ea0ccaec50628d83c4c5ae1c6383cee2f
Leonardo Lopes <leonardolivelopes2@gmail.com>
2023-12-04 21:07:38 -0300

    moving pieces

--- src/App.java
+++ src/App.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 import chess.ChessMath;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
         ChessMath chessMatch = new ChessMath();
-        UI.printBoard(chessMatch.getPieces());
+
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
--- src/chess/ChessMath.java
+++ src/chess/ChessMath.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -22,6 +24,28 @@ public class ChessMath {
     }
 
     return mat;
+  }
+
+  public ChessPiece performChessMove(ChessPosition sourcPosition, ChessPosition targetPosition) {
+    Position source = sourcPosition.toPosition();
+    Position target = targetPosition.toPosition();
+    validateSourcePosition(source);
+    Piece capturedPiece = makeMove(source, target);
+    return (ChessPiece) capturedPiece;
+  }
+
+  private Piece makeMove(Position source, Position target) {
+    Piece p = board.removePiece(source);
+    // se houver peça no lugar de destino, é a peça capturada
+    Piece caputuredPiece = board.removePiece(target);
+    board.placePiece(p, target);
+    return caputuredPiece;
+  } 
+
+  private void validateSourcePosition(Position position) {
+    if (!board.thereIsAPiece(position)) {
+      throw new ChessException("There is no piece on source position");
+    }
   }
 
   private void placeNewPiece(char column, int row, ChessPiece piece) {
